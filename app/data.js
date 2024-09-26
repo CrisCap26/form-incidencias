c8336bf53a6e8d9b6ca8425eca3362af0e539d16
Cris <cristopher.26hp@gmail.com>
2024-09-26 00:19:15 -0600

    Agregando componentes necesarios para el form

---
+++ app/data.js
@@ -0,0 +1,24 @@
+export const animals = [
+    {key: "cat", label: "Cat"},
+    {key: "dog", label: "Dog"},
+    {key: "elephant", label: "Elephant"},
+    {key: "lion", label: "Lion"},
+    {key: "tiger", label: "Tiger"},
+    {key: "giraffe", label: "Giraffe"},
+    {key: "dolphin", label: "Dolphin"},
+    {key: "penguin", label: "Penguin"},
+    {key: "zebra", label: "Zebra"},
+    {key: "shark", label: "Shark"},
+    {key: "whale", label: "Whale"},
+    {key: "otter", label: "Otter"},
+    {key: "crocodile", label: "Crocodile"}
+];
+
+export const tipoIncidencias = [
+    {key: 1, label:"Vacaciones"},
+    {key: 2, label:"Permiso sin goce"},
+    {key: 3, label:"Permiso de llegar tarde"},
+    {key: 4, label:"Permiso de salir temprano"},
+    {key: 5, label:"Paternidad"},
+    {key: 6, label:"Incapacidad"}
+]
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,118 @@
-import Image from "next/image";
+// import { Button } from '@nextui-org/button';
 
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <div className='p-5'>
+//       <Button color='primary'>Add task</Button>
+//     </div>
+//   )
+// }
+'use client'
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Checkbox, Input, Link, DatePicker } from "@nextui-org/react";
+import React, { useState } from "react";
+import { Select, SelectItem } from "@nextui-org/react";
+import { tipoIncidencias, animals } from './data'
+import { RangeCalendar } from "@nextui-org/react";
+import { today, getLocalTimeZone } from "@internationalized/date";
+// import {MailIcon} from './MailIcon.jsx';
+// import {LockIcon} from './LockIcon.jsx';
+
+export default function App() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+  const [tipoDeIncidencia, setTipoDeIncidencia] = useState<number | undefined>(undefined);
+  const handleSignIn = () => {
+    // Aquí puedes manejar el envío de datos
+    console.log({ email, password, rememberMe, tipoDeIncidencia });
+    // Puedes agregar la lógica para autenticar al usuario aquí
+  };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <>
+      <Button onPress={onOpen} color="primary">Nueva Incidencia</Button>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        placement="top-center"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Crear incidencia</ModalHeader>
+              <ModalBody>
+                <Select
+                  items={tipoIncidencias}
+                  label="Tipo de incidencia"
+                  placeholder="Selecciona un tipo de incidencia"
+                  className="max-w-xs"
+                  value={tipoDeIncidencia}
+                  onChange={(e) => setTipoDeIncidencia(Number(e.target.value))}
+                >
+                  {(incidencia) => <SelectItem key={incidencia.key}>{incidencia.label}</SelectItem>}
+                </Select>
+                {
+                  tipoDeIncidencia !== undefined ?
+                    tipoDeIncidencia === 1 ?
+                      <RangeCalendar
+                        aria-label="Date (Min Date Value)"
+                        minValue={today(getLocalTimeZone())}
+                      />
+                      :
+                      <DatePicker label="Selecciona fecha" className="max-w-[284px]" />
+                    :
+                    <></>
+                }
+                {/* <Input
+                  autoFocus
+                  // endContent={
+                  //   <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                  // }
+                  label="Email"
+                  placeholder="Enter your email"
+                  variant="bordered"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                  // endContent={
+                  //   <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                  // }
+                  label="Password"
+                  placeholder="Enter your password"
+                  type="password"
+                  variant="bordered"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                /> */}
+                <div className="flex py-2 px-1 justify-between">
+                  <Checkbox
+                    classNames={{
+                      label: "text-small",
+                    }}
+                    isSelected={rememberMe}
+                    onChange={() => setRememberMe((prev) => !prev)}
+                  >
+                    Remember me
+                  </Checkbox>
+                  <Link color="primary" href="#" size="sm">
+                    Forgot password?
+                  </Link>
+                </div>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="flat" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={() => { handleSignIn(); onClose(); }}>
+                  Sign in
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
